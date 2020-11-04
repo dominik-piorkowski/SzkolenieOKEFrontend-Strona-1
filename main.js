02fb2349ccd454f70fc5e3f2de0548b549b8790f
Dominik Piórkowski <73492222+dominik-piorkowski@users.noreply.github.com>
2020-11-04 19:54:16 +0100

    Lesson 2

---
+++ main.js
@@ -0,0 +1,21 @@
+// console.log("HelloWorld");
+
+// const myVar = 5;
+// console.log(myVar);
+
+// function multiply() {
+//     console.log(4*12)
+// }
+
+// multiply();
+
+// const add = () => {
+//     console.log(100+321)
+// }
+// add()
+
+// const addWithArgs = (firstNumber, secondNumber) => {
+//     console.log(firstNumber + secondNumber)
+// }
+
+// addWithArgs(500,9)
